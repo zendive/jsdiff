d1252adb5be1f1513daad9e0330f0bed34c53399
🕷 <3756473+zendive@users.noreply.github.com>
2024-01-04 00:36:25 +0200

    chore: remove autogenerated files from git [1]

--- bundle/js/firefox/jsdiff-background.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var t={};let n=new Set;chrome.runtime.onConnect.addListener(e=>{e.name==="jsdiff-devtools-page-connect"&&(n.add(e),e.onDisconnect.addListener(()=>{n.delete(e)}))}),chrome.runtime.onMessage.addListener(e=>{for(const s of n)s.postMessage(e)})})();
--- bundle/js/jsdiff-devtools.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";var a={};chrome.devtools.inspectedWindow.tabId!==null&&chrome.devtools.panels.create("JSDiff","/bundle/img/panel-icon28.png","/bundle/jsdiff-panel.html",e=>{typeof e.onSearch?.addListener=="function"&&e.onSearch.addListener(async(n,s)=>{await chrome.runtime.sendMessage({source:"jsdiff-devtools-to-panel-search",params:{cmd:n,query:s}})})})})();
