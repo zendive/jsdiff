8d021e87770af626284ed2830aedf8eb579fdec4
🕷 <3756473+zendive@users.noreply.github.com>
2024-01-08 07:06:06 +0200

    update dependencies

--- webpack.config.ts
+++ webpack.config.ts
@@ -57,6 +57,7 @@ export default function (
       new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: 'false',
         __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
       }),
     ],
 
