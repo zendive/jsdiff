f22d618492f637bdeb967fc1b9dc5166ec343839
🕷️ <3756473+zendive@users.noreply.github.com>
2026-05-19 12:26:20 +0300

    adopt `@awasm/noble`
    Signed-off-by: 🕷️ <3756473+zendive@users.noreply.github.com>

--- src/api/toolkit.ts
+++ src/api/toolkit.ts
@@ -1,12 +1,14 @@
-import { sha256 } from '@noble/hashes/sha2.js';
-import { utf8ToBytes } from '@noble/hashes/utils.js';
+import { sha256 } from '@awasm/noble/js.js';
 
 export function hasValue(o: unknown): boolean {
   return undefined !== o && null !== o;
 }
 
+const te = /*@__PURE__*/ (() => new TextEncoder())();
+const uint8buffer = /*@__PURE__*/ (() => new Uint8Array(sha256.outputLen))();
+
 export function hashString(str: string) {
-  return sha256(utf8ToBytes(str)).toHex();
+  return sha256(te.encode(str), { out: uint8buffer }).toHex();
 }
 
 export function isSearchKeyboardEvent(e: KeyboardEvent) {
