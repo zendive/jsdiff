ae40b0f0953448ef299b54e43ccfcfabe1afa038
🕷️ <3756473+zendive@users.noreply.github.com>
2026-09-05 21:07:58 +0300

    infra: track build number
    Signed-off-by: 🕷️ <3756473+zendive@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ CHROME_ZIP = "extension.chrome-$(CHROME_MANIFEST_VERSION).zip"
 FIREFOX_MANIFEST = ./manifest.firefox.json
 FIREFOX_MANIFEST_VERSION != jq -j '.version' $(FIREFOX_MANIFEST)
 FIREFOX_ZIP = "extension.firefox-$(FIREFOX_MANIFEST_VERSION).zip"
-DENO_DEV = BUILD_MODE=development deno run --watch --allow-env --allow-read --allow-run
+DENO_DEV = BUILD_MODE=development deno run --watch --allow-env --allow-read --allow-write="./manifest.json" --allow-run
 DENO_PROD = BUILD_MODE=production deno run --allow-env --allow-read --allow-run
 OUTPUT_DIR = ./public/
 BUILD_DIR = ./public/build/
--- build.ts
+++ build.ts
@@ -9,6 +9,7 @@ const isProd = Deno.env.get('BUILD_MODE') === 'production';
 const isFirefox = isProd && Deno.args.includes('--x-is-firefox');
 const buildMode = isProd ? 'production' : 'development';
 const logLevel = isProd ? 'warning' : 'debug';
+const buildVersion = generateBuildVersion(isProd, manifest);
 const buildOptions: BuildOptions = {
   plugins: [
     vue3Plugin({
@@ -29,7 +30,7 @@ const buildOptions: BuildOptions = {
     __VUE_PROD_DEVTOOLS__: 'false',
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
     __development__: `${!isProd}`,
-    __app_version__: `"${manifest.version}"`,
+    __app_version__: `"${buildVersion}"`,
     __app_homepage__: `"${manifest.homepage_url}"`,
     __firefox__: `${isFirefox}`,
   },
@@ -54,4 +55,33 @@ if (isProd) {
 } else {
   const ctx = await context(buildOptions);
   await ctx.watch();
+}
+
+/**
+ * Get or regenerate build number and update current manifest copy
+ * Assuming current manifest is a writable copy of a relevant extension manifest
+ * * in production build - just return authored version
+ * * in development build - keep incrementing each build 4th
+ */
+function generateBuildVersion(isProd: boolean, manifest: { version: string }) {
+  if (isProd) return manifest.version;
+
+  // major minor patch revision
+  const aMaMiPaBu = manifest.version.split('.');
+
+  if (!aMaMiPaBu[3]) {
+    aMaMiPaBu[3] = '0';
+  } else {
+    // The integers must be between 0 and 65535, inclusive
+    const CAP = 65536;
+    const buildNum = (Number.parseInt(aMaMiPaBu[3], 10) + 1) % CAP;
+    aMaMiPaBu[3] = String(buildNum);
+  }
+
+  manifest.version = aMaMiPaBu.join('.');
+
+  Deno.writeTextFile('./manifest.json', JSON.stringify(manifest, null, 2))
+    .catch(console.error);
+
+  return manifest.version;
 }
